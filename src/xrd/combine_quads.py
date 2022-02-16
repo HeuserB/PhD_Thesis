b284170fc60923168874f614d95474a84646043c
HeuserB <benheuser@t-online.de>
2022-02-16 11:09:52 +0100

    HELIOS plotting added

--- src/xrd/combine_quads.py
+++ src/xrd/combine_quads.py
@@ -223,6 +223,39 @@ def merge_four_quads(quad_data, scale):
         
     return combined_data
 
+def combine_patterns(patterns):
+    x_min = []
+    for pattern in patterns:
+        x = pattern[0]
+        x_min.append(np.min(x))
+
+    sorted_pattern_ind = np.argsort(x_min)
+
+    pattern = patterns[sorted_pattern_ind[0]]
+    for ind in sorted_pattern_ind[1:]:
+        x1, y1 = pattern[0] , pattern[1] 
+        x2, y2 = patterns[ind][0], patterns[ind][0]
+
+        pattern2_interp1d = interp1d(x2, y2, kind='linear')
+
+        overlap_ind_pattern1 = np.where((x1 <= np.max(x2)) & (x1 >= np.min(x2)))[0]
+        left_ind_pattern1 = np.where((x1 <= np.min(x2)))[0]
+        right_ind_pattern2 = np.where((x2 >= np.max(x1)))[0]
+
+        combined_x1 = x1[left_ind_pattern1]
+        combined_y1 = y1[left_ind_pattern1]
+        combined_x2 = x1[overlap_ind_pattern1]
+        combined_y2 = (y1[overlap_ind_pattern1] + pattern2_interp1d(combined_x2)) / 2
+        combined_x3 = x2[right_ind_pattern2]
+        combined_y3 = y2[right_ind_pattern2]
+
+        combined_x = np.hstack((combined_x1, combined_x2, combined_x3))
+        combined_y = np.hstack((combined_y1, combined_y2, combined_y3))
+
+        pattern = np.vstack([combined_x, combined_y])
+
+    return pattern
+
 
 if __name__=="__main__":
     files_quad = [sys.argv[1],sys.argv[2], sys.argv[3],sys.argv[4]]
