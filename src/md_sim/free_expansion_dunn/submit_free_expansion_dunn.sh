78756f03a8f9102c877062b77c05c93606dddbd6
HeuserB <benheuser@t-online.de>
2022-07-28 09:19:01 +0200

    dunn expansion added

--- src/md_sim/free_expansion_dunn/submit_free_expansion_dunn.sh
+++ src/md_sim/free_expansion_dunn/submit_free_expansion_dunn.sh
@@ -6,7 +6,7 @@ mkdir inputs_free_expansion/done/
 python3 create_inputs.py
 tar -czvf free_expansion.tar.gz inputs_free_expansion
 rm -r inputs_free_expansion
-scp free_expansion.tar.gz bh326@titan1.hpc.uni-rostock.de:/home/bh326/free_expansion_airebo_m/
+scp free_expansion.tar.gz bh326@titan1.hpc.uni-rostock.de:/home/bh326/free_expansion_dunn/
 rm free_expansion.tar.gz
 
-ssh bh326@titan1.hpc.uni-rostock.de 'cd ~/free_expansion_airebo_m/  && tar -xf free_expansion.tar.gz'
+ssh bh326@titan1.hpc.uni-rostock.de 'cd ~/free_expansion_dunn/  && tar -xf free_expansion.tar.gz'
